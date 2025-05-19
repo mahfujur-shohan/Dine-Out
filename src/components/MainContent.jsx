6a9393dd0e4f1c425b41e2d8e3df3709e3170b34
Mahfujur Rahman <mahfujurrahmancityu@gmail.com>
2025-05-19 23:13:30 +0600

    dynamic items added

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import CreateOrder from "./CreateOrder";
 import OrderInfo from "./OrderInfo";
 
 export default function MainContent() {
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  function handleTotalPrice(item) {
+    const updatedPrice = totalPrice + item.price;
+    setTotalPrice(updatedPrice);
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6 flex-grow">
-      <CreateOrder />
+      <CreateOrder
+        totalPrice={totalPrice}
+        handleTotalPrice={handleTotalPrice}
+      />
       <OrderInfo />
     </div>
   );
---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+import chicken from "../assets/chicken.svg";
+import hamburger from "../assets/hamburger.svg";
+import pizza from "../assets/pizza.svg";
+import sandwich from "../assets/submarine.svg";
+
+export const data = [
+  {
+    id: 1,
+    itemName: "Hamburger",
+    price: 300,
+    imgUrl: hamburger,
+  },
+  {
+    id: 2,
+    itemName: "Chicken Nuggets",
+    price: 350,
+    imgUrl: chicken,
+  },
+  {
+    id: 3,
+    itemName: "Submarine Sandwich",
+    price: 100,
+    imgUrl: sandwich,
+  },
+  {
+    id: 4,
+    itemName: "Pizza slice",
+    price: 400,
+    imgUrl: pizza,
+  },
+];
